11cc8bc7fd45b9b728f7f86546388adc60d7b857
unknown <amir15bfk@gmail.com>
2021-01-11 14:46:15 +0100

    hello world

---
+++ hello world/hello_world.pas
@@ -0,0 +1,21 @@
+program hello_world;
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}{$IFDEF UseCThreads}
+  cthreads,
+  {$ENDIF}{$ENDIF}
+  Classes
+  { you can add units after this };
+
+begin
+
+
+    writeln('hello world');
+    readln;
+
+
+
+end.
+
